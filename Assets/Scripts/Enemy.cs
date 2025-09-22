7701bd7dff42ba6e37d572ef73ed5d40bfd2dd10
ChloeSVillaranda <chloesvillaranda@gmail.com>
2025-09-22 15:26:18 -0600

    enemies added, spawns, and interacts with bullets

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public float speed = 2f;
+    public int health = 1;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+
+        if (transform.position.y < -6f) // If enemy goes off screen
+        {
+            Destroy(gameObject); // Destroy it
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Bullet")) {
+            health--;
+            Destroy(collision.gameObject); // Destroy the bullet
+
+            if (health <= 0) {
+                Destroy(gameObject); // Destroy enemy if health is 0
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float spawnInterval = 2f;
+    public float spawnRangeX = 8f;
+
+    private float timer;
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= spawnInterval) {
+            SpawnEnemy();
+            timer = 0f;
+        }
+    }
+    void SpawnEnemy() {
+        Vector2 spawnPos = new Vector2(Random.Range(-spawnRangeX, spawnRangeX), 6f);
+        Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+    }
+}
