8482b322745e83300c3a1a5e1c89e608cc642044
ChloeSVillaranda <chloesvillaranda@gmail.com>
2025-09-23 10:14:43 -0600

    feat: working scene switcher for menu options

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneSwitcher : MonoBehaviour
 {
-
     public void OpenScene(string sceneName) {
         SceneManager.LoadScene(sceneName);
     }
+    public void QuitGame() {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false; // Stops Play Mode in the Editor
+        #else
+                Application.Quit(); // Quits the application in a build
+        #endif
+    }
+
     // Start is called before the first frame update
     void Start()
     {
